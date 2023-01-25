817fc724545611b421873c629b89918a31009a18
DaviBavaresco <100875783+DaviBavaresco@users.noreply.github.com>
2023-01-25 18:59:16 -0300

    teste 5

--- Aula1.py
+++ Aula1.py
@@ -4,8 +4,20 @@ print("*********************************")
 print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
+print("qual o nivel de dificuldade? ")
+print("(1)Fácil, (2)Médio, (3)Difícel")
+nivel = int(input("Qual nivel escolhe? "))
+
 numero_secreto = random.randrange(1, 101)
-total_de_tentativas = 3
+total_de_tentativas = 0
+pontos = 1000
+
+if(nivel == 1):
+    total_de_tentativas = 15
+elif(nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
 
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
@@ -22,12 +34,14 @@ for rodada in range(1, total_de_tentativas + 1):
     menor   = chute < numero_secreto
 
     if(acertou):
-        print("Você acertou!")
+        print("Você acertou e fez {}".format(pontos))
         break
     else:
         if(maior):
             print("Você errou! O seu chute foi maior do que o número secreto.")
         elif(menor):
             print("Você errou! O seu chute foi menor do que o número secreto.")
+            pontos_perdidos = abs(numero_secreto - chute)
+            pontos -= pontos_perdidos
 
 print("Fim do jogo")
